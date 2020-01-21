42e2932492e91435fc724fdbf956ab07f83de713
Luc Mosser <luc.mosser86@orange.fr>
2020-01-21 10:01:37 +0100

    routing works

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get 'pages/contact'
-  get 'pages/about'
-  get 'pages/home'
-  get 'pages/books'
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'books', to: 'pages#books'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
