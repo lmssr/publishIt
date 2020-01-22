c23755dd9ec5b8b28d34b5e99993e23dcb10be77
Luc Mosser <luc.mosser86@orange.fr>
2020-01-22 10:11:53 +0100

    book section work

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,11 @@ class BooksController < ApplicationController
     BOOKS = {
     1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
     2 => { name: "Sushi Samba", address: "City, London", category: "japanese" },
-    3 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+    3 => { name: "Sushi Samba", address: "City, London", category: "japanese" },
+    4 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    5 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    6 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    7 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" }
   }
 
   def index
